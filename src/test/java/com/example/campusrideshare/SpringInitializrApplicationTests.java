8cd717576a25cfbcb179264bc01846b8c29abfaf
jamila455 <jrahimi@mail.mccneb.edu>
2025-11-15 14:22:30 -0600

    Initial Spring Boot setup for Campus Ride Share

---
+++ src/test/java/com/example/campusrideshare/SpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campusrideshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
